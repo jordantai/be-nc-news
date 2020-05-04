943d48283d02f276f419b1f43612beb6afd201d4
jordan <contact@creativecuckoo.com>
2020-05-04 12:16:31 +0100

    initial setup complete

---
+++ db/data/index.js
@@ -0,0 +1,9 @@
+//set default environment to development if ENV not set
+const ENV = process.env.NODE_ENV || "development";
+
+const data = {
+  development: require("./development-data"),
+  test: require("./test-data"),
+};
+
+module.exports = data[ENV];
