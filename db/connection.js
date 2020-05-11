88dbdfa67db95e3a0cdaf8100ffbb554328bdb45
jordan <contact@creativecuckoo.com>
2020-05-11 23:12:24 +0100

    scripts added and dbconfig changed to connect with heroku for production

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,14 @@
-const dbConfig = require("../knexfile");
+const ENV = process.env.NODE_ENV || "development";
 
 const connection = require("knex");
+
+const dbConfig =
+  ENV === "production"
+    ? {
+        client: "pg",
+        connection: process.env,
+        DATABASE_URL,
+      }
+    : require("../knexfile");
 
 module.exports = connection(dbConfig);
--- db/data/index.js
+++ db/data/index.js
@@ -4,6 +4,7 @@ const ENV = process.env.NODE_ENV || "development";
 const data = {
   development: require("./development-data"),
   test: require("./test-data"),
+  production: require("./development-data"),
 };
 
 module.exports = data[ENV];
