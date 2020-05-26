d9253abb25aa46321afb3c20759d4cc790eeb98a
jordan <contact@creativecuckoo.com>
2020-05-26 09:43:23 +0100

    cors express dependency added

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const express = require("express");
-const apiRouter = require("./routes/api.router");
+const express = require('express');
+const apiRouter = require('./routes/api.router');
+const cors = require('cors');
+app.use(cors());
 const app = express();
 const {
   handlePSQLErrors,
   handleCustomErrors,
   handleInternalErrors,
   send404,
-} = require("./errors");
+} = require('./errors');
 
 app.use(express.json());
-app.use("/api", apiRouter);
+app.use('/api', apiRouter);
 app.use(send404);
 
 app.use(handlePSQLErrors);
